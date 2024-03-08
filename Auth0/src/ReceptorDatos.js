6bc0b73d30f8f242216a678b5e9bd045587b7b84
Alberto Crespo Murillo <acrespoq@alumnos.unex.es>
2024-03-08 23:40:18 +0100

    Translation of the url thanks to the Spilab server by both ibm and aws. The translated code is shown. An intermediate server has been created to communicate the React request with the Spilab server due to problems with the CORS policy.

--- Auth0/src/ReceptorDatos.js
+++ Auth0/src/ReceptorDatos.js
@@ -6,6 +6,9 @@ const ReceptorDatos = () => {
   const location = useLocation();
   const [id, setId] = useState(null);
   const [link, setLink] = useState(null);
+  const [awsCode, setAwsCode] = useState(null);
+  const [ibmCode, setIbmCode] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const getParamsFromUrl = () => {
     const searchParams = new URLSearchParams(location.search);
@@ -22,6 +25,25 @@ const ReceptorDatos = () => {
     }
   };
 
+  const translateToPython = async () => {
+    if (!link) return;
+
+    setLoading(true);
+    try {
+      // Realizar la solicitud a la API de AWS
+      const awsResponse = await axios.get('http://localhost:4246/code/aws', { headers: { 'x-url': link } });
+      setAwsCode(awsResponse.data.code.join('\n'));
+
+      // Realizar la solicitud a la API de IBM
+      const ibmResponse = await axios.get('http://localhost:4246/code/ibm', { headers: { 'x-url': link } });
+      setIbmCode(ibmResponse.data.code.join('\n'));
+    } catch (error) {
+      console.error('Error translating to Python:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     getParamsFromUrl();
   }, [location]);
@@ -32,17 +54,26 @@ const ReceptorDatos = () => {
     }
   }, [id]);
 
-  if (!id) {
-    return <div>No se pudo obtener el ID correctamente</div>;
-  }
-
-  if (!link) {
-    return <div>Cargando...</div>;
-  }
+  useEffect(() => {
+    translateToPython();
+  }, [link]);
 
   return (
     <div>
-      <p>Link asociado al ID {id}: {link}</p>
+      {loading ? (
+        <div>Traduciendo a Python...</div>
+      ) : (
+        <div>
+          <div>
+            <h2>Código AWS:</h2>
+            <pre>{awsCode}</pre>
+          </div>
+          <div>
+            <h2>Código IBM:</h2>
+            <pre>{ibmCode}</pre>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
